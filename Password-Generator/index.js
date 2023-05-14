0c5e77eff1b910cfea4563b27c24fb0b252bc11d
Osamah Ahmed Abdullah Al-baidhani <96604157+Ozy2022@users.noreply.github.com>
2023-05-14 11:32:46 +0800

    Add files via upload

---
+++ Password-Generator/index.js
@@ -0,0 +1,41 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T",
+"U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
+"t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#",
+"$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+let passwordLength = 12
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter()         
+    }
+    return randomPassword
+}
+
+const generatedPasswordOne = generateRandomPassword()
+
+let Te1 = document.getElementById("t1")
+let Te2 = document.getElementById("t2")
+
+function tes12() {
+    
+    Te1.value = generateRandomPassword()
+    Te2.value = generateRandomPassword()
+    
+    return generateRandomPassword()
+    
+}
+
+document.getElementById("generate").addEventListener("click", tes12);
+
+
+
+
